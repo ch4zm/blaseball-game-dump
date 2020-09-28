e491e7fa11e5e2cb287e3372bf29c901f12aebe3
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-09-27 19:14:24 -0700

    fix version flag, add python api call

--- game_dump/command.py
+++ game_dump/command.py
@@ -3,7 +3,7 @@ import os
 import json
 import argparse
 from .view import View
-
+from .util import CaptureStdout
 
 def main(sysargs = sys.argv[1:]):
 
@@ -25,19 +25,28 @@ def main(sysargs = sys.argv[1:]):
     if len(sysargs)==0:
         p.print_help()
         exit(0)
-
-    # Parse arguments
-    options = p.parse_args(sysargs)
-
-    # If the user asked for the version,
-    # print the version number and exit.
-    if options.version:
+    elif '-v' in sysargs or '--version' in sysargs:
+        # If the user asked for the version,
+        # print the version number and exit.
+        # (Note: this is done separate from
+        # argparse, because otherwise the user
+        # has to ALSO provide a game ID to get
+        # the --version flag to work. ugh.)
         from . import _program, __version__
         print(_program, __version__)
         sys.exit(0)
 
+    # Parse arguments
+    options = p.parse_args(sysargs)
+
     v = View(options)
     v.show()
+
+
+def game_dump(sysargs):
+    with CaptureStdout() as so:
+        main(sysargs)
+    return str(so)
 
 
 if __name__ == '__main__':
